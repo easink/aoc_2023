e7669493141d4f9dff7f0b16445c65bdf30d6a3a
Andreas Rydbrink <andreas.rydbrink@gmail.com>
2023-12-02 09:07:12 +0100

    day01 forgot to add correct answer to test

--- test/day01_test.exs
+++ test/day01_test.exs
@@ -8,6 +8,6 @@ defmodule AoCDay01Test do
 
   test "part2" do
     assert AoC.Day01.part2("inputs/day01.part2.sample") == 281
-    assert AoC.Day01.part2("inputs/day01") == 213_958
+    assert AoC.Day01.part2("inputs/day01") == 54076
   end
 end
